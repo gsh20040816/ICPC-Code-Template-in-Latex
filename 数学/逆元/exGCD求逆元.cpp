1d956dbe5f427f648775cc0a2b47e7306fa8ad09
gsh <gsh20040816@gmail.com>
2023-11-03 19:09:08 +0800

    add something

--- 数学/逆元/exGCD求逆元.cpp
+++ 数学/逆元/exGCD求逆元.cpp
@@ -1,9 +1,3 @@
-void exGCD(ll a,ll b,ll&x,ll&y)
-{
-	if(!b){x=1,y=0;return;}
-	exGCD(b,a%b,y,x);
-	y-=a/b*x;
-}
 int inv(int a,int P)
 {
 	ll x,y;
--- 数学/逆元/快速幂求逆元.cpp
+++ 数学/逆元/快速幂求逆元.cpp
@@ -1,12 +1,1 @@
-int pow(int a,int b,int P)
-{
-	int ans=1;
-	while(b)
-	{
-		if(b&1)ans=1ll*ans*a%P;
-		a=1ll*a*a%P;
-		b>>=1;
-	}
-	return ans;
-}
 int inv(int a,int P){return pow(a,P-2,P);}
